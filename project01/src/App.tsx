253a5cffd8201ceaa6034de6b92e472ebec56270
NICHOLAS <nicholasfelipe361@gmail.com>
2025-10-07 00:16:05 -0300

    Adicionado incrementos visuais

--- project01/src/App.tsx
+++ project01/src/App.tsx
@@ -7,25 +7,29 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="min-w-screen min-h-screen">
+  <>
+    <div className="w-screen h-screen p-4">
       <div>
-        <h1 className='m-auto text-center mt-4 text-3xl font-bold text-white'>LOGIN</h1>
+        <h1 className='h-1/6 m-auto text-center text-3xl font-bold text-white'>LOGIN</h1>
       </div>
-      <div>
-        <div>
+      <div className='h-5/6 flex flex-col justify-center'>
+        <div className='bg-orange-100 h-2/5 w-3xs md:w-2/5 md:h-1/3 m-auto rounded-lg flex flex-col justify-center'>
+        <div className='text-center'>
           <p>
             Email
           </p>
-          <input type="email" />
+          <input type="email" className='bg-white rounded-sm'/>
         </div>
-        <div>
+        <div className='text-center'>
           <p>
             Password
           </p>
-          <input type="password" name="" id="" />
+          <input type="password" className='bg-white rounded-sm'/>
         </div>
       </div>
+      </div>
     </div>
+  </>
   )
 }
 

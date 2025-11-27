09dc233efb1b5d2973129318d6f095875605089a
nicholas-amarante <nicholasfelipe361@gmail.com>
2025-11-27 00:33:40 -0300

    Modificacoes design e adicao de ruido

--- Finance-Web/src/pages/Home.tsx
+++ Finance-Web/src/pages/Home.tsx
@@ -1,10 +1,36 @@
+import '../App.css';
+
 function Home(){
     return(
     <>
-        <div>
-            <h1>
-                FODASE
-            </h1>
+        <div className="bg-gradient-to-bl from-[#404658] to-[#8A96BE] h-screen w-screen
+        noise
+        ">
+            <div className="">
+                <div className="">
+                    <div className="flex justify-end">
+                        <p className="
+                        mr-4
+                        pt-0.5 pb-0.5 pl-1 pr-1
+                        border-2
+                        border-
+                        ">
+                            Sign in
+                        </p>
+                        <p className="
+                        ml-4 mr-4
+                        pt-0.5 pb-0.5 pl-1 pr-1
+                        border-2 border-solid 
+                        border-b-black
+                        ">
+                        Sign up
+                        </p>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     </>
 )
